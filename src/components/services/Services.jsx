52192b0bff753cfe544762e90dbaa62456890dd3
Soufiane Oumbarek <150067195+Oumbarek-Soufiane@users.noreply.github.com>
2025-04-17 11:40:23 +0200

    Update Services.jsx

--- src/components/services/Services.jsx
+++ src/components/services/Services.jsx
@@ -27,7 +27,7 @@ const Services = () => {
         </div>
 
         <div className="service__offer">
-          <a href="https://github.com/Oumbarek-Soufiane/School_Elkarama_App">
+          <a href="https://github.com/Oumbarek-Soufiane/School_Elkarama_App" target="_blank">
             <img src={serviceImg1} alt="service2" className="service__2" />
           </a>
           <h2>School Management</h2>
@@ -35,18 +35,18 @@ const Services = () => {
             «Institution Academic Connect: Your Gateway to Crea-tivity and
             Innovation El karama Boussaid 
           </p>
-          <a href="https://github.com/Oumbarek-Soufiane/School_Elkarama_App">
+          <a href="https://github.com/Oumbarek-Soufiane/School_Elkarama_App" target="_blank">
             <i class="bx bxs-share"></i>
           </a>
         </div>
 
         <div className="service__offer">
-          <a href="https://github.com/Oumbarek-Soufiane/project-hackathon">
+          <a href="https://github.com/Oumbarek-Soufiane/project-hackathon"  target="_blank">
             <img src={serviceImg2} alt="service3" className="service__3" />
           </a>
           <h2>Hackathon Project</h2>
           <p> «Smart Garbage Using Arduino And C++ Language </p>
-          <a href="https://github.com/Oumbarek-Soufiane/project-hackathon">
+          <a href="https://github.com/Oumbarek-Soufiane/project-hackathon" target="_blank">
             <i class="bx bxs-share"></i>
           </a>
         </div>
@@ -55,18 +55,19 @@ const Services = () => {
           <img src={serviceImg4} alt="service4" className="service__4" />
           <h2>Chefchaoun Project</h2>
           <p> « Professional Composition Using Adobe Photoshop </p>
-          <a download="" href={serviceImg4}>
+          <a download="" href={serviceImg4} target="_blank">
             <i class="bx bxs-share"></i>
           </a>
         </div>
           <div className="service__offer">
-          <a href="https://www.figma.com/proto/CsG3AGJGc5ZykHu2tZwkYP/projet-ux-ui?node-id=2-2">
-            <img src={serviceImg5} alt="service5" className="service__3" />
+          <a href="https://www.figma.com/proto/CsG3AGJGc5ZykHu2tZwkYP/projet-ux-ui?node-id=2-2" target="_blank">
+            <img src={serviceImg5} alt="service5" className="service__3"  />
           </a>
           <h2>UX / UI Projet</h2>
           <p> «Projet Plate Forme Musicale ( Figma ) </p>
-          <a href="https://www.figma.com/proto/CsG3AGJGc5ZykHu2tZwkYP/projet-ux-ui?node-id=2-2">
+          <a href="https://www.figma.com/proto/CsG3AGJGc5ZykHu2tZwkYP/projet-ux-ui?node-id=2-2" target="_blank">
             <i class="bx bxs-share"></i>
+            
           </a>
         </div>
       </div>
